1a3f8f8adc3a9bd5a14697d1cb1fbd07505e568e
meed ghoufrane <baloncesmeed@gmail.com>
2024-03-28 15:27:56 +0000

    add auth

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,34 @@
+<template>
+    <v-container>
+      <v-form @submit.prevent="submitForm">
+        <v-text-field v-model="formData.email" label="Email" type="email" required></v-text-field>
+        <v-text-field v-model="formData.password" label="Password" type="password" required></v-text-field>
+        <v-btn type="submit" color="primary">Login</v-btn>
+      </v-form>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        formData: {
+          email: '',
+          password: ''
+        }
+      };
+    },
+    methods: {
+      submitForm() {
+        // Handle form submission here
+        console.log('Form submitted:', this.formData);
+        // You can make an HTTP request to your server to handle login
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* Add custom styles here if needed */
+  </style>
+  
---
+++ src/components/RegistrationForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-form @submit.prevent="submitForm">
+      <v-text-field v-model="formData.name" label="Name" required></v-text-field>
+      <v-text-field v-model="formData.email" label="Email" type="email" required></v-text-field>
+      <v-text-field v-model="formData.password" label="Password" type="password" required></v-text-field>
+      <v-btn type="submit" color="primary">Register</v-btn>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+        email: '',
+        password: ''
+      }
+    };
+  },
+  methods: {
+    submitForm() {
+      // Handle form submission here
+      console.log('Form submitted:', this.formData);
+      // You can make an HTTP request to your server to handle registration
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add custom styles here if needed */
+</style>
